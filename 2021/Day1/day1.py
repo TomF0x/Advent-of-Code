62645a3a063d139be1af6d65c7460356970725df
TomFox <tom007video@gmail.com>
2021-12-01 11:17:51 +0100

    Second start day1

--- 2021/Day1/day1.py
+++ 2021/Day1/day1.py
@@ -3,3 +3,10 @@ def FirstStar(lst):
 
 
 print(FirstStar(open("data.txt", "r").read().split("\n")))
+
+
+def SecondStar(lst):
+    return sum([1 if int(lst[i])+int(lst[i+1])+int(lst[i+2]) < int(lst[i + 1])+int(lst[i + 2]) + int(lst[i+3]) else 0 for i in range(len(lst) - 3)])
+
+
+print(SecondStar(open("data.txt", "r").read().split("\n")))
