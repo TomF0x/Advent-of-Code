d32cb0a5d11bf0e0cc1ce1d1445de1be79e27341
TomFox <tom007video@gmail.com>
2021-12-06 10:30:09 +0100

    Second star day 6

--- 2021/Day6/day6.py
+++ 2021/Day6/day6.py
@@ -1,5 +1,5 @@
 def FirstStar(lst):
-    for _ in range(80):
+    for _ in range(5):
         for i in range(len(lst)):
             if int(lst[i]) > 0:
                 lst[i] = str(int(lst[i])-1)
@@ -10,3 +10,21 @@ def FirstStar(lst):
 
 
 FirstStar(open("data.txt", "r").read().split(","))
+
+
+def SecondStar(lst):
+    dic = {str(i): 0 for i in range(-1, 9)}
+    temp = {str(i): 0 for i in range(-1, 9)}
+    for number in lst:
+        dic[number] += 1
+    for _ in range(256):
+        for i in range(8, -1, -1):
+            temp[str(i-1)] = dic[str(i)]
+        temp["8"] = temp["-1"]
+        temp["6"] += temp["-1"]
+        temp["-1"] = 0
+        dic = temp.copy()
+    print(sum(dic.values()))
+
+
+SecondStar(open("data.txt", "r").read().split(","))
